684c66fa8a48d6d54f4fea9193ccbd2acd4e082b
Yuki-SHI0726 <szx0726@gmail.com>
2022-07-13 00:10:12 -0700

    Initial UE project setup

---
+++ Source/ProjectDog/ProjectDogGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectDogGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDOG_API AProjectDogGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
