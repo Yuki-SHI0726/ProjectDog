6c7b35e810e8aff7774fd8b428156ceda0dd9559
Yuki-SHI0726 <szx0726@gmail.com>
2022-07-16 00:44:04 -0700

    feat: Configured First Person Shooter Package to the game

---
+++ Source/ProjectDog/Characters/Player/FP_FirstPersonCharacter.h
@@ -0,0 +1,103 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "FP_FirstPersonCharacter.generated.h"
+
+class UInputComponent;
+class UCameraComponent;
+class USkeletalMeshComponent;
+class USoundBase;
+class UAnimMontage;
+
+UCLASS(config=Game)
+class AFP_FirstPersonCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFP_FirstPersonCharacter();
+
+protected:
+	/** First person shooter members*/
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(BlueprintReadWrite, Category=Mesh)
+	USkeletalMeshComponent* Mesh1P;
+
+	/** Gun mesh */
+	UPROPERTY(BlueprintReadWrite, Category = Mesh)
+	USkeletalMeshComponent* FP_Gun;
+
+	/** First person camera */
+	UPROPERTY(BlueprintReadWrite, Category = Camera)
+	UCameraComponent* FirstPersonCameraComponent;
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Camera)
+	float BaseLookUpRate;
+
+	/** Gun muzzle's offset from the characters location */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	FVector GunOffset;
+
+	/** Sound to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
+	USoundBase* FireSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	UAnimMontage* FireAnimation;
+
+	/* This is when calculating the trace to determine what the weapon has hit */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float WeaponRange;
+	
+	/* This is multiplied by the direction vector when the weapon trace hits something to apply velocity to the component that is hit */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay)
+	float WeaponDamage;
+
+	/** Gameplay members */
+
+
+protected:
+	/** Fires a virtual projectile. */
+	void OnFire();
+
+	/** Handles moving forward/backward */
+	void MoveForward(float Val);
+
+	/** Handles strafing movement, left and right */
+	void MoveRight(float Val);
+
+	/**
+	 * Called via input to turn at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	/* 
+	 * Performs a trace between two points
+	 * 
+	 * @param	StartTrace	Trace starting point
+	 * @param	EndTrac		Trace end point
+	 * @returns FHitResult returns a struct containing trace result - who/what the trace hit etc.
+	 */
+	FHitResult WeaponTrace(const FVector& StartTrace, const FVector& EndTrace) const;
+
+	// APawn interface
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputComponent) override;
+	// End of APawn interface
+};
+
---
+++ Source/ProjectDog/Core/ProjectDogGameMode.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ProjectDogGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDOG_API AProjectDogGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
